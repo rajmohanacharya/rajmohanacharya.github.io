55fc13a60d82878b371fbee48894a64feb8f3b10
Raj Mohan Acharya <rajmohanacharya@protonmail.com>
2025-10-07 17:54:54 +0530

    Update script.js

--- experience/script.js
+++ experience/script.js
@@ -31,16 +31,30 @@ srtop.reveal('.experience .timeline .container',{interval: 400});
 
 
 // Start of Tawk.to Live Chat
+//var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+//(function(){
+//var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+//s1.async=true;
+//s1.src='https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+//s1.charset='UTF-8';
+//s1.setAttribute('crossorigin','*');
+//s0.parentNode.insertBefore(s1,s0);
+//})();
+// End of Tawk.to Live Chat
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+s1.src='https://embed.tawk.to/68e504f39cf08c194dc2cdaf/1j6v9mmob';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
-// End of Tawk.to Live Chat
+</script>
+<!--End of Tawk.to Script-->
 
 
 // disable developer mode
@@ -74,3 +88,4 @@ function(){
     }
 
 });
+
