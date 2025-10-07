dfc53fbc29490ac7af3fbae27c0077d40065a380
Raj Mohan Acharya <rajmohanacharya@protonmail.com>
2025-10-07 18:05:06 +0530

    Update script.js

--- experience/script.js
+++ experience/script.js
@@ -42,8 +42,8 @@ srtop.reveal('.experience .timeline .container',{interval: 400});
 //})();
 // End of Tawk.to Live Chat
 
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
+//<!--Start of Tawk.to Script-->
+//<script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -53,8 +53,8 @@ s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
-</script>
-<!--End of Tawk.to Script-->
+//</script>
+//<!--End of Tawk.to Script-->
 
 
 // disable developer mode
@@ -88,4 +88,5 @@ function(){
     }
 
 });
+
 
